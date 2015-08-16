ca932f17c1a4df9df31339e34bd68508704cbc26
Joshua Marshall <catchjosh@gmail.com>
2015-08-15 23:35:24 -0500

    Incrementing version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="debfolder",
-    version="0.1.1",
+    version="0.1.2",
     description="A few tools for building Debian packages for Python.",
     author="Josh Marshall",
     author_email="catchjosh@gmail.com",
